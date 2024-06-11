293bffa13115f22c6b36d693749d36875c9162ae
takumaBTC6 <takuma_suzuki_ab@mail.toytota.co.jp>
2024-06-11 16:12:20 +0900

    add : 要素の追加削除にアニメ追加

--- frontend/src/components/Notes.tsx
+++ frontend/src/components/Notes.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card, Group, Text, Button, Badge } from "@mantine/core";
 import { Res } from "./grobals";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 interface Props {
   list: Res[];
@@ -11,35 +12,39 @@ interface Props {
 const Notes = ({ list, selectId, setSelectId }: Props) => {
   return (
     <>
-      {list.map((note) => {
-        const viewTime =
-          note.updatedAt !== null
-            ? new Date(note.updatedAt)
-            : new Date(note.createdAt);
-        const formattedTime = viewTime.toLocaleString();
-        return (
-          <Card
-            bg={note.id === selectId ? "powderblue" : "white"}
-            key={note.id}
-            mt={3}
-            mb={3}
-            onClick={() => setSelectId(note.id)}
-          >
-            <Group justify="space-between" mt="md" mb="xs">
-              <Text fw={500}>{note.title}</Text>
-              <Badge color="pink">On Sale</Badge>
-            </Group>
+      <TransitionGroup>
+        {list.map((note) => {
+          const viewTime =
+            note.updatedAt !== null
+              ? new Date(note.updatedAt)
+              : new Date(note.createdAt);
+          const formattedTime = viewTime.toLocaleString();
+          return (
+            <CSSTransition key={note.id} timeout={500} classNames="note-item">
+              <Card
+                bg={note.id === selectId ? "powderblue" : "white"}
+                key={note.id}
+                mt={3}
+                mb={3}
+                onClick={() => setSelectId(note.id)}
+              >
+                <Group justify="space-between" mt="md" mb="xs">
+                  <Text fw={500}>{note.title}</Text>
+                  <Badge color="pink">On Sale</Badge>
+                </Group>
 
-            <Text size="sm" c="dimmed">
-              {note.text}
-            </Text>
+                <Text size="sm" c="dimmed">
+                  {note.text}
+                </Text>
 
-            <Button color="blue" fullWidth mt="md" radius="md">
-              {formattedTime}
-            </Button>
-          </Card>
-        );
-      })}
+                <Button color="blue" fullWidth mt="md" radius="md">
+                  {formattedTime}
+                </Button>
+              </Card>
+            </CSSTransition>
+          );
+        })}
+      </TransitionGroup>
     </>
   );
 };
